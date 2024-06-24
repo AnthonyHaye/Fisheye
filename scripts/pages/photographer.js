0a275958a447d8bcc52e2de41c46b58d6863d097
Anthony Hayé <anthony.haye49@laposte.net>
2024-06-24 10:14:09 +0200

    premier commit

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -8,6 +8,7 @@ import { updateTotalLikesAndPrice} from '../utils/updateTotalLikesAndPrice.js'
 
 async function main() {
     const params = new URLSearchParams(window.location.search);
+    console.log(params);
     const photographerId = parseInt(params.get('id'));
 
     try {
@@ -46,50 +47,5 @@ async function main() {
     } catch (error) {
         console.error("Fetch error: ", error);
     }
-}
-
-// async function main() {
-//     const params = new URLSearchParams(window.location.search);
-//     const photographerId = parseInt(params.get('id'));
-
-//     try {
-//         const response = await fetch("./data/photographers.json");
-//         if (!response.ok) {
-//             throw new Error("Network response was not ok");
-//         }
-//         const data = await response.json();
-//         const photographerData = data.photographers.find(p => p.id === photographerId);
-//         const mediaData = data.media.filter(m => m.photographerId === photographerId);
-
-//         if (!photographerData) {
-//             throw new Error("Photographe pas trouvé");
-//         }
-
-//         // Instanciation de la classe Photographer
-//         const photographer = new Photographer(photographerData);
-
-//         // Instanciation des objets Media
-//         const mediaObjects = mediaData.map(m => new Media(m, photographer.name));
-//         const mediaList = mediaObjects.map(media => ({
-//             media: media.image || media.video,
-//             title: media.title
-//         }));
-
-//         // Mise à jour de la section des détails du photographe
-//         updatePhotographerHeader(photographer);
-
-//         // Mise à jour de la section des médias du photographe
-//         updatePhotographerMedia(mediaObjects, mediaList, photographer.name);
-        
-//         // Configuration du menu déroulant pour le tri
-//         setupDropdown(mediaObjects, updatePhotographerMedia);
-
-//         // mise à jour  total likes and price
-//         updateTotalLikesAndPrice(mediaObjects, photographer.price);
-
-//     } catch (error) {
-//         console.error("Fetch error: ", error);
-//     }
-// }
-
+}     
 main();
--- scripts/templates/photographerTemplate.js
+++ scripts/templates/photographerTemplate.js
@@ -2,48 +2,49 @@
 import { createTextElement } from '../utils/createElements.js';
 
 export function photographerTemplate(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    
+    const { name, portrait, city, country, tagline, price } = data;    
 
     const picture = `assets/images/Sample Photos/Photographers ID Photos/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement('article');
-        article.className = "Photographe_card";
+            function getUserCardDOM() {
+                const article = document.createElement('article');
+                article.className = "Photographe_card";
 
-        const lienElement = document.createElement("a");
-        lienElement.className = "vers_page_photographe";
-        lienElement.href = `photographer.html?id=${data.id}`;
+                const lienElement = document.createElement("a");
+                lienElement.className = "vers_page_photographe";
+                lienElement.href = `photographer.html?id=${data.id}`;
 
-        const img = document.createElement('img');
-        img.setAttribute("src", picture);
-        img.className = "portrait_photographe";
-        img.setAttribute("alt", name);
+                const img = document.createElement('img');
+                img.setAttribute("src", picture);
+                img.className = "portrait_photographe";
+                img.setAttribute("alt", name);
 
-        const h2 = document.createElement('h2');
-        h2.className = "name_photographe";
-        h2.textContent = name;
+                const h2 = document.createElement('h2');
+                h2.className = "name_photographe";
+                h2.textContent = name;
 
-        const paysPhotographeElement = document.createElement("div");
-        paysPhotographeElement.className = "paysPhotographe";
-        const cityPhotographe = createTextElement("p", `${city}, `);
-        const countryPhotographe = createTextElement("p", country);
+                const paysPhotographeElement = document.createElement("div");
+                paysPhotographeElement.className = "paysPhotographe";
+                const cityPhotographe = createTextElement("p", `${city}, `);
+                const countryPhotographe = createTextElement("p", country);
 
-        const taglinePhotographe = createTextElement("p", tagline, "taglinePhotographe");
-        const prixPhotographe = createTextElement("p", `${price} €/jour`, "prixPhotographe");
+                const taglinePhotographe = createTextElement("p", tagline, "taglinePhotographe");
+                const prixPhotographe = createTextElement("p", `${price} €/jour`, "prixPhotographe");
 
-        paysPhotographeElement.appendChild(cityPhotographe);
-        paysPhotographeElement.appendChild(countryPhotographe);
+                paysPhotographeElement.appendChild(cityPhotographe);
+                paysPhotographeElement.appendChild(countryPhotographe);
 
-        lienElement.appendChild(img);
-        lienElement.appendChild(h2);
+                lienElement.appendChild(img);
+                lienElement.appendChild(h2);
 
-        article.appendChild(lienElement);
-        article.appendChild(paysPhotographeElement);
-        article.appendChild(taglinePhotographe);
-        article.appendChild(prixPhotographe);
+                article.appendChild(lienElement);
+                article.appendChild(paysPhotographeElement);
+                article.appendChild(taglinePhotographe);
+                article.appendChild(prixPhotographe);
 
-        return article;
-    }
+                return article;
+            }
 
     return { name, picture, getUserCardDOM }
 }
